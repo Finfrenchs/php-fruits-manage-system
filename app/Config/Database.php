290360cb852c118480d30501895409a645b1aeda
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2026-04-23 23:29:54 +0700

    Force SSL transport for TiDB Cloud

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -97,7 +97,8 @@ class Database
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES   => false,
-                    // Aktifkan SSL untuk koneksi Cloud
+                    // Paksa menggunakan SSL (Wajib untuk TiDB Cloud Serverless)
+                    PDO::MYSQL_ATTR_SSL_CA       => '', 
                     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
                 ]);
             } catch (PDOException $e) {
